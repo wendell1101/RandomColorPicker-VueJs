100c5427978786b70f55ef2835aad8f5b2cc3939
Wendell Suazo <wendellchansuazo11@gmail.com>
2020-12-16 10:03:39 +0800

    add copy functionlity

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
-const app = Vue.createApp ({
+const app = Vue.createApp({
     // data
-    data () {
-        return{
-            "x" : 0,
-            "y" : 0,
-            "z" : 0,
-            "rgb" : 'rgb(0,0,0)',
-            "hexadecimal" : '#fff',
+    data() {
+        return {
+            "x": 0,
+            "y": 0,
+            "z": 0,
+            "rgb": 'rgb(0,0,0)',
+            "hexadecimal": '#fff',
         }
     },
     // methods
-    methods : {
+    methods: {
         // pick color 
         pickColor(e) {
             const colorBox = document.getElementById('color-box');
             // choose random color
-            randomNum1 = Math.floor(Math.random()*255);
-            randomNum2 = Math.floor(Math.random()*255);
-            randomNum3 = Math.floor(Math.random()*255);
-            setTimeout(()=>{
-                this.x = randomNum1;              
-            },1500);
+            randomNum1 = Math.floor(Math.random() * 255);
+            randomNum2 = Math.floor(Math.random() * 255);
+            randomNum3 = Math.floor(Math.random() * 255);
+            setTimeout(() => {
+                this.x = randomNum1;
+            }, 1500);
 
-            setTimeout(()=>{
+            setTimeout(() => {
                 this.y = randomNum2;
-            },1500);
+            }, 1500);
 
-            setTimeout(()=>{
-                this.z = randomNum3;              
-            },1500);
-           
-            let color = `rgb(${this.x},${this.y},${this.z})`;    
-             
+            setTimeout(() => {
+                this.z = randomNum3;
+            }, 1500);
+
+            let color = `rgb(${this.x},${this.y},${this.z})`;
+
             colorBox.style.background = color;
             let rgb = document.getElementById('rgb');
             let hexadecimal = document.getElementById('hex');
@@ -41,15 +41,15 @@ const app = Vue.createApp ({
                 // remove mousemove event listener
                 this.hasColor = true;
                 // rgb = color;
-                color = rgb.innerHTML = color;
+                color = rgb.value = color;
                 let hex = this.rgbToHex(this.x, this.y, this.z);
-                hexadecimal.innerHTML = hex;
+                hexadecimal.value = hex;
                 this.rgb = color;
-                this.hexadecimal = color; 
+                this.hexadecimal = color;
                 this.showBgColor();
             });
             // set the color if mouse leave
-            e.target.addEventListener('mouseleave', () =>{
+            e.target.addEventListener('mouseleave', () => {
                 e.target.style.background = this.hexadecimal;
             })
         },
@@ -61,11 +61,41 @@ const app = Vue.createApp ({
             var hex = c.toString(16);
             return hex.length == 1 ? "0" + hex : hex;
         },
-        
+
         // function to show color in mini box in the dom
-        showBgColor(){
-            document.querySelector('.mini-color').style.background=`${this.hexadecimal}`;
-        }
+        showBgColor() {
+            document.querySelector('.mini-color').style.background = `${this.hexadecimal}`;
+        },
+        // copy 
+        copyRgb() {
+            /* Get the text field */
+            let copyText = document.getElementById("rgb");
+
+            /* Select the text field */
+            copyText.select();
+            copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+            /* Copy the text inside the text field */
+            document.execCommand("copy");
+
+            /* Alert the copied text */
+            alert("Copied color: " + copyText.value);
+        },
+        // copy 
+        copyHex() {
+            /* Get the text field */
+            let copyText = document.getElementById("hex");
+
+            /* Select the text field */
+            copyText.select();
+            copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+            /* Copy the text inside the text field */
+            document.execCommand("copy");
+
+            /* Alert the copied text */
+            alert("Copied color: " + copyText.value);
+        },
     },
 });
 
